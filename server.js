560731dbbc2f3d16a58095aba9f0d4b7e4b2e3b6
ivopalomeque <ivopalomeque1@gmail.com>
2024-09-19 21:02:17 -0300

    Separating connection and model logic

--- server.js
+++ server.js
@@ -1,84 +1,18 @@
-const { Sequelize, DataTypes } = require('sequelize')
-process.loadEnvFile()
-
-const { DBUSER, PASSWORD, HOST, DATABASE } = process.env
-
-const sequelize = new Sequelize(DATABASE, DBUSER, PASSWORD, {
-host: HOST,
-dialect: 'mysql',
-})
-
-const Product = sequelize.define(
-    'Product',
-    {
-      productID: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-      },
-      productName: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      SupplierID: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        default: 1,
-      },
-      CategoryID: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        default: 1,
-      },
-      QuantityPerUnit: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        default: 'N/A',
-      },
-      UnitPrice: {
-        type: DataTypes.DOUBLE,
-        allowNull: false,
-        default: 0.0,
-      },
-      UnitsInStock: {
-        type: DataTypes.SMALLINT,
-        allowNull: false,
-        default: 0,
-      },
-      UnitsOnOrder: {
-        type: DataTypes.SMALLINT,
-        allowNull: false,
-        default: 0,
-      },
-      ReorderLevel: {
-        type: DataTypes.SMALLINT,
-        allowNull: false,
-        default: 1,
-      },
-      Discontinued: {
-        type: DataTypes.BOOLEAN,
-        allowNull: false,
-        default: 0,
-      },
-    },
-    {
-      tableName: 'Products',
-      timestamps: false,
-    }
-  )
+const { Product } = require('./src/modelos/product')
+const { sequelize } = require('./src/conexion/connection')
 
 const main = async () => {
-    try {
+  try {
     await sequelize.authenticate()
     console.log('Conexión establecida con exito ! =)')
     await Product.sync()
-    const products = await Product.findAll();   
-    const productsData = products.map((p)=> p.dataValues);   
+    const products = await Product.findAll()
+    const productsData = products.map((p) => p.dataValues)
     console.table(productsData)
-    } catch (error) {
-    console.error('No me pude conectar porque sucedió:', error)
-    } finally{
-        sequelize.close()
-    }
- }
-    main()
+  } catch (error) {
+    console.error('No me pude conectar porque sucedió: ', error)
+  } finally {
+    sequelize.close()
+  }
+}
+main()
---
+++ src/connection/connection.js
@@ -0,0 +1,11 @@
+const { Sequelize } = require('sequelize')
+
+process.loadEnvFile()
+const { DBUSER, PASSWORD, HOST, DATABASE } = process.env
+
+const sequelize = new Sequelize(DATABASE, DBUSER, PASSWORD, {
+  host: HOST,
+  dialect: 'mysql',
+})
+
+module.exports = { sequelize }
